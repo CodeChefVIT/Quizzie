f737be6f5fa954cd671c64a34ea414614121bcef
jugalbhatt123 <jugalbhatt3@gmail.com>
2020-06-22 16:44:28 +0530

    cors pt.2

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ app.use((req, res, next) => {
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header(
 		"Access-Control-Allow-Headers",
-		"Origin, X-Requested-With, Content-Type, Accept, Authorization"
+		"Origin, X-Requested-With, Content-Type, Accept, Authorization,auth-token"
 	);
 	if (req.method === "OPTIONS") {
 		res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
