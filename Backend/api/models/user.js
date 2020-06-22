0e43a93c5ebc338adb790521c4eedee9670a0d81
jugalbhatt123 <jugalbhatt3@gmail.com>
2020-06-22 22:34:43 +0530

    a small change quiz route and user model

--- Backend/api/models/user.js
+++ Backend/api/models/user.js
@@ -3,40 +3,38 @@ const Quiz = require("./quiz");
 const Questions = require("./question");
 const Admin = require("./admin");
 
-
 const studentSchema = mongoose.Schema({
-  _id: mongoose.Schema.Types.ObjectId,
-  googleId:{
-    type:String
-  },
-  name: { type: String,},
-    
-  userType: { type: String, default: "User" },
-  email: {
-    type: String,
-    match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/,
-  },
-  mobileNumber: {
-    type: Number,
-    match: /^([7-9][0-9]{9})$/g,
-  },
-  password: { type: String },
-  quizzesGiven: [
-    {
-      quizId: { type: mongoose.Schema.Types.ObjectId, ref: "Quiz" },
-      marks: { type: Number},
-      responses: [],
-    },
-  ],
-  quizzesEnrolled:[
-    {
-      quizId:{type:mongoose.Schema.Types.ObjectId,
-      ref:"Quiz"}
-    }
-  ],
-  token:{
-    type:String
-  }
+	_id: mongoose.Schema.Types.ObjectId,
+	googleId: {
+		type: String,
+	},
+	name: { type: String },
+
+	userType: { type: String, default: "User" },
+	email: {
+		type: String,
+		match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/,
+	},
+	mobileNumber: {
+		type: Number,
+		match: /^([7-9][0-9]{9})$/g,
+	},
+	password: { type: String },
+	quizzesGiven: [
+		{
+			quizId: { type: mongoose.Schema.Types.ObjectId, ref: "Quiz" },
+			marks: { type: Number },
+			responses: [],
+		},
+	],
+	quizzesEnrolled: [
+		{
+			quizId: { type: mongoose.Schema.Types.ObjectId, ref: "Quiz" },
+		},
+	],
+	token: {
+		type: String,
+	},
 });
 
 module.exports = mongoose.model("Student", studentSchema);
