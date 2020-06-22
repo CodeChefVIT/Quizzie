df6feddd6b997b58ec9f36d2dc8964acc44f8fee
jugalbhatt123 <jugalbhatt3@gmail.com>
2020-06-22 15:23:03 +0530

    stupid error

--- Backend/api/config/passport-setup.js
+++ Backend/api/config/passport-setup.js
@@ -1,6 +1,5 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('./keys');
 const User = require('../models/user');
 const jwt = require('jsonwebtoken')
 const mongoose = require('mongoose')
