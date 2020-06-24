d5632b592f09f33563ee06ef294b94137ed092bb
Sarthak <sarthakbh321@gmail.com>
2020-06-24 18:17:15 +0530

    display quizzes

--- Frontend/src/components/QuizzesSection.js
+++ Frontend/src/components/QuizzesSection.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import './QuizzesSection.css';
 import axios from "axios";
+import { GridList, GridListTile, GridListTileBar, Typography } from "@material-ui/core";
 
 function QuizzesSection(props) {
 	const [userType, setUserType] = useState(props.type);
@@ -31,8 +32,22 @@ function QuizzesSection(props) {
 
 	return (
 		<div className="quizzes-section">
+			<Typography variant="h5" className="up-quizzes">Upcoming Quizzes</Typography>
 			{quizzes.length === 0? <p>Sorry! No quizzes available at the moment!</p>
-			:null }
+			: 
+				<div className="quiz-list root">
+					<GridList cols={3} className="grid-list">
+						{quizzes.map((quiz) => (
+							<GridListTile key={quiz._id} className="quiz-tile">
+								<img src="../CC LOGO-01.svg" />
+								<GridListTileBar 
+									title={quiz.quizName} 
+									subtitle={`By: ${quiz.adminId.name}`}/>
+							</GridListTile>
+						))}
+					</GridList>
+				</div> 
+			}
 		</div>
 	)
 }
