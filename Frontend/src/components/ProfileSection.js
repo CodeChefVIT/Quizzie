565a898bce6e84a32cabf70c17b19adca08de300
Sarthak <sarthakbh321@gmail.com>
2020-06-26 03:21:29 +0530

    profiles text size reduced

--- Frontend/src/components/ProfileSection.js
+++ Frontend/src/components/ProfileSection.js
@@ -8,15 +8,15 @@ function ProfileSection(props) {
 
 	return (
 		<div className="profile-section">
-			<Typography variant="h5" className="profile-param">Name: <span className="profile-data">{profile.name}</span></Typography>
-			<Typography variant="h5" className="profile-param">E-mail: <span className="profile-data">{profile.email}</span></Typography>
-			<Typography variant="h5" className="profile-param">Phone Number: <span className="profile-data">{profile.mobileNumber}</span></Typography>
+			<Typography variant="h6" className="profile-param">Name: <span className="profile-data">{profile.name}</span></Typography>
+			<Typography variant="h6" className="profile-param">E-mail: <span className="profile-data">{profile.email}</span></Typography>
+			<Typography variant="h6" className="profile-param">Phone Number: <span className="profile-data">{profile.mobileNumber}</span></Typography>
 			{userType === "user" ?
 				<div>
-					<Typography variant="h5" className="profile-param">Quizzes Enrolled: <span className="profile-data">{profile.quizzesEnrolled.length}</span></Typography>
-					<Typography variant="h5" className="profile-param">Quizzes Completed: <span className="profile-data">{profile.quizzesGiven.length}</span></Typography>
+					<Typography variant="h6" className="profile-param">Quizzes Enrolled: <span className="profile-data">{profile.quizzesEnrolled.length}</span></Typography>
+					<Typography variant="h6" className="profile-param">Quizzes Completed: <span className="profile-data">{profile.quizzesGiven.length}</span></Typography>
 				</div>
-			: <Typography variant="h5" className="profile-param">Quizzes Created: <span className="profile-data">{profile.quizzes.length}</span></Typography> }
+			: <Typography variant="h6" className="profile-param">Quizzes Created: <span className="profile-data">{profile.quizzes.length}</span></Typography> }
 		</div>
 	)
 }
