d28e7b4c68c9793fd2d99f931f1dd6ad709553c7
Sarthak <sarthakbh321@gmail.com>
2020-06-28 02:20:48 +0530

    add questions linked

--- Frontend/src/pages/EditQuiz.js
+++ Frontend/src/pages/EditQuiz.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from "react";
 import './EditQuiz.css';
-import { Container, Typography, Button } from "@material-ui/core";
+import { Container, Typography, Button, Dialog, Grid, InputLabel, Select, MenuItem } from "@material-ui/core";
 import { Create } from "@material-ui/icons";
 import axios from "axios";
 import Loading from "./Loading";
+import TextInput from "../components/TextInput";
 
 function EditQuiz(props) {
 	const quizId = props.match.params.id;
@@ -14,7 +15,81 @@ function EditQuiz(props) {
 	const [quizDetails, setQuizDetails] = useState({});
 	const [quizQuestions, setQuizQuestions] = useState([]);
 
+	const [questionModal, setQuestionModal] = useState(false);
+	const [newQuestion, setNewQuestion] = useState("");
+
+	const [option1, setOption1] = useState("");
+	const [option2, setOption2] = useState("");
+	const [option3, setOption3] = useState("");
+	const [option4, setOption4] = useState("");
+	const [correctOption, setCorrectOption] = useState("nonono");
+
+	const onCloseHandle = () => {
+		setQuestionModal(false);
+	}
+
+	const onQuestionChange = (event) => {
+		setNewQuestion(event.target.value);
+	}
+
+	const handleOptionChange1 = (event) => {
+		setOption1(event.target.value);
+	}
+	const handleOptionChange2 = (event) => {
+		setOption2(event.target.value);
+	}
+	const handleOptionChange3 = (event) => {
+		setOption3(event.target.value);
+	}
+	const handleOptionChange4 = (event) => {
+		setOption4(event.target.value);
+	}
+
+	const handleCorrectOption = (event) => {
+		setCorrectOption(event.target.value);
+	}
+
+	const clearModal = () => {
+		setNewQuestion("");
+		setOption1("");
+		setOption2("");
+		setOption3("");
+		setOption4("");
+	}
+
+	const handleQuestionSubmit = async () => {
+		let token = localStorage.getItem("authToken");
+		let url = `https://quizzie-api.herokuapp.com/question/add`;
+
+		let options = [
+			{"text": option1},{"text": option2},{"text": option3},{"text": option4},
+		]
+
+		let data = {
+			"quizId": quizId,
+			"description": newQuestion,
+			"options": options,
+			"correctAnswer": correctOption
+		}
+
+		try {
+			await axios.post(url, data, {
+				headers: {
+					"auth-token": token
+				}
+			}).then(res => {
+				clearModal();
+				getQuizDetails();
+				setQuestionModal(false);
+			})
+		} catch(error) {
+			console.log(error);
+		}
+
+	}
+
 	const getQuizDetails = async () => {
+		setLoading(true);
 		let token = localStorage.getItem("authToken");
 		let url = `https://quizzie-api.herokuapp.com/quiz/${quizId}`;
 
@@ -75,12 +150,78 @@ function EditQuiz(props) {
 					<Typography variant="h4" className="quiz-questions-head">Questions</Typography>
 					<div className="quiz-questions-display">
 						<div className="add-question-bar">
-							<Button variant="filled" className="add-question-btn">Add a question</Button>
+							<Button variant="filled" className="add-question-btn" onClick={() => setQuestionModal(true)}>Add a question</Button>
 						</div>
 						{quizQuestions.length === 0? <p style={{ textAlign: 'center' }}>No questions added yet!</p>
 						: null}
 					</div>
 				</div>
+				<Dialog open={questionModal} onClose={onCloseHandle} aria-labelledby="add-question-modal"
+					PaperProps={{ style: { backgroundColor: 'white', color: '#333', minWidth: '50%' } }}
+					style={{ width: '100%' }}>
+					<Typography variant="h6" style={{textAlign: 'center', margin: '2% 5%'}}>New Question</Typography>
+					<div className="new-question-form">
+						<TextInput
+							className="new-ques-input"
+							variant="outlined"
+							label="Question Text"
+							value={newQuestion}
+							onChange={onQuestionChange} />
+						<hr style={{width: '100%', marginBottom: '3%'}}/>
+						<Grid container spacing={1}>
+							<Grid item xs={12} sm={6}>
+							<TextInput
+								className="new-ques-input"
+								variant="outlined"
+								label="Option 1"
+								value={option1}
+								onChange={handleOptionChange1} />
+							</Grid>
+							<Grid item xs={12} sm={6}>
+							<TextInput
+								className="new-ques-input"
+								variant="outlined"
+								label="Option 2"
+								value={option2}
+								onChange={handleOptionChange2} />
+							</Grid>
+						</Grid>
+						<Grid container spacing={1}>
+							<Grid item xs={12} sm={6}>
+							<TextInput
+								className="new-ques-input"
+								variant="outlined"
+								label="Option 3"
+								value={option3}
+								onChange={handleOptionChange3} />
+							</Grid>
+							<Grid item xs={12} sm={6}>
+							<TextInput
+								className="new-ques-input"
+								variant="outlined"
+								label="Option 4"
+								value={option4}
+								onChange={handleOptionChange4} />
+							</Grid>
+						</Grid>
+						<hr style={{width: '100%', marginBottom: '3%'}}/>
+						<InputLabel id="correct-option">Correct Option</InputLabel>
+						<Select
+							className="correct-answer-select"
+							style={{width: '50%'}}
+							labelId="correct-option"
+							value={correctOption}
+							onChange={handleCorrectOption} 
+						>
+							<MenuItem value="nonono">None</MenuItem>
+							{option1.trim().length !== 0? <MenuItem value={option1}>{option1}</MenuItem> :null }
+							{option2.trim().length !== 0? <MenuItem value={option2}>{option2}</MenuItem> :null } 
+							{option3.trim().length !== 0? <MenuItem value={option3}>{option3}</MenuItem> :null }
+							{option4.trim().length !== 0? <MenuItem value={option4}>{option4}</MenuItem> :null }
+						</Select>
+						<Button variant="filled" className="add-question-submit" onClick={handleQuestionSubmit}>Add Question</Button>
+					</div>
+				</Dialog>
 			</Container>
 		)
 	}
