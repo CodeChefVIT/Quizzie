3e3e8f8a5c5b61c1ac4075b3951d7b0e69b85afe
jugalbhatt123 <jugalbhatt3@gmail.com>
2020-07-15 07:49:24 +0530

    forgot password email and reset password route done

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ app.use(passport.session());
 /////Rate Limiter
 const limiter = rateLimit({
 	windowMs: 15 * 60 * 1000, // 15 minutes
-	max: 100 // limit each IP to 100 requests per windowMs
+	max: 150 // limit each IP to 100 requests per windowMs
   });
    
   //  apply to all requests
