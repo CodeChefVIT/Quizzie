e86d5d01a230c723eb03277f5f2085df7c52395e
Manny <mquizhpidev@gmail.com>
2020-09-30 18:11:47 -0400

    add couldn't to error massage

--- Frontend/src/pages/ForgotPassword.js
+++ Frontend/src/pages/ForgotPassword.js
@@ -212,7 +212,7 @@ function ForgotPassword(props) {
 					<br />
 					{notSent ? (
 						<Alert severity="error" color="warning">
-							We can't find a user with that e-mail address.
+							We couldn't find a user with that e-mail address.
 						</Alert>
 					) : null}
 					<form className="form">
